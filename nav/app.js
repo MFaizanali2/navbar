64434d886ed65a84e591fb8a5c83944492e5c5fc
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-01 01:21:19 +0500

    Update app.js

---
+++ nav/app.js
@@ -0,0 +1,13 @@
+let navbar = document.querySelector(".nav")
+navbar.innerHTML = `
+ <nav>
+                <h1>SpaceX</h1>
+                <ul>
+                    <li> <a href="#">Home</a></li>
+                    <li> <a href="#">About</a></li>
+                    <li> <a href="#">Contact us</a></li>
+                    <li> <a href="#">Service</a></li>
+                    <li> <a href="#">portfolio</a></li>
+                </ul>
+                <button type="button">Subscribe</button>
+            </nav>`
