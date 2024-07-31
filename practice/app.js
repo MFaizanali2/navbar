a2290b64a8cbd8b70ffd42becd2bb160ab228e33
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-01 01:24:35 +0500

    Update app.js

---
+++ practice/app.js
@@ -0,0 +1,41 @@
+let box = document.getElementById(".box");
+box.innerHTML = `
+ <H1> DENVER DAHIL </H1> 
+ <H3> Acount Manager </H3>
+ +1 55555 5555 5555
+ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp; 
+ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp; 
+ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;
+denver . dehil example
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ san francisco,CA
+ <h2>Experience</h2>
+ <hr>
+ <b>key account maneger </b>
+ <br>
+ lauzor
+ <br>
+ 2016-ongoing
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+san francisco,CA
+<br>
+<p>
+ lauzan is a leading manufacture ,dsigner, and supplier off beraring liner motion 
+ product prisious bearing spendles , sell and services
+ reponsible for bussines development with key account with main focus is energy
+ power utilize and industiries</p>
+ <br>
+ <ul>
+ <li> perioned opf fixed + mobile services in spain 2012 </li>
+<li> achives 126% revenue attainment in H2 (Jan Jun 2015)</li> 
+<li> increfised in fashion brand pc reveue by 90 % yoymainly wiouth
+    the help of our shopping compagion </li> 
+    <li> achive to list a satisfaction of 9 + 10 with the
+         usagr of oraclefor one top account  </li>  
+  </ul>  
+<b> senior account maneger </b>
+<br> `
