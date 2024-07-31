3de38c00e6b1be2e4aab2b36f550692046f1eb5b
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-01 01:04:18 +0500

    Update app.js

---
+++ footer1/app.js
@@ -0,0 +1,51 @@
+let box = document.querySelector(".box");
+box.innerHTML = `
+<footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="footer_col">
+                        <h4> Company </h4>
+                        <ul>
+                             <li><a href="@"> about us </a></li>
+                            <li><a href="@"> our service </a></li>
+                            <li><a href="@"> privicy policy </a></li>
+                             <li><a href="@"> artificial program </a></li>
+                        </ul>
+                    </div>
+
+
+                    <div class="footer_col">
+                        <h4> Get help </h4>
+                        <ul>
+                            <li><a href="@"> FAQ </a></li>
+                            <li><a href="@"> shipping </a></li>
+                            <li><a href="@"> returns </a></li>
+                            <li><a href="@"> order status </a></li>
+                            <li><a href="@">payment options</a></li>
+                        </ul>
+                    </div>
+
+
+                    <div class="footer_col">
+                        <h4> Online shop </h4>
+                        <ul>
+                            <li><a href="@"> watch </a></li>
+                            <li><a href="@"> bags </a></li>
+                            <li><a href="@"> shoes </a></li>
+                            <li><a href="@"> dress </a></li>
+                        </ul>
+                    </div>
+
+
+                    <div class="footer_col">
+                        <h4> Follow us </h4>
+                        <div class="social_link">
+                            <a href="@"><i class="fa-brands fa-facebook"></i></a>
+                            <a href="@"><i class="fa-brands fa-instagram"> </i></a>
+                            <a href="@"><i class="fa-brands fa-twitter"> </i></a>
+                            <a href="@"><i class="fa-brands fa-linkedin-in"> </i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>`
